53bc2a7194433cad55fc90aca40a3576183c1bf8
anmol-narain <anmolnarain83@gmail.com>
2023-06-07 14:11:55 +0530

    second commit

---
+++ RANDOM_OCEAN_FACTS/facts.js
@@ -0,0 +1,246 @@
+const facts = [`The oceans cover over 70% of Earth's surface.`,
+
+`The oceans are nearly as old as the Earth itself. Scientists believe that the oceans contained at least some of their water more than 4 billion years ago.`,
+
+`If all the salt in the ocean was removed and poured onto the continents of Earth, it would cover them to a depth of 500 feet.`,
+
+`Thalassophobia is the name for intense fear of the sea or, more generally, the fear of deep, dark water.`,
+
+`Explorers of the sea and its mysteries are sometimes called "oceanauts."`,
+
+`Earth's oceans contain roughly 320 million cubic miles of water.`,
+
+`The United Nations passed the "Convention on the Law of the Sea" in 1994 and is now the recognized governing body in all legal matters concerning the world's oceans.`,
+
+`Genesis, the first book of the Bible, represents the oceans as being older than the dry land. It is stated that God created Heaven and Earth, and the Earth is initially portrayed as a dark and formless water.`,
+
+`Historically, people spoke of seven oceans in order to preserve the ancient notion of the "seven seas."  Truthfully, any division of the oceans is arbitrary since there is only a single global sea.`,
+
+`The biggest ocean oil spill in history occurred in 1991, during the Gulf War. Iraqi soldiers retreating from Kuwait opened oil wells and pipelines in an attempt to slow U.S. troops; 240 million gallons of oil spilled into the Persian Gulf, creating a slick roughly the size of Hawaii.`,
+
+`We know more about the surface of the moon than the floor of the sea.`,
+
+`The names "Pacific," "Atlantic," and "Indian" were formally accepted worldwide in 1845.`,
+
+`While the words "ocean" and "sea" are used interchangeably, a sea is technically a smaller body of salt water that is part of a larger ocean.`,
+
+`Ninety-seven percent of Earth's water is contained in the oceans.`,
+
+`All life on Earth began in the ocean.`,
+
+`The effects of human pollution and over-fishing have dramatically changed the world's oceans over the past hundred years. The long-term effects of waste dumping and commercial fishing remain to be seen.`,
+
+`In his work Meteorology, the Ancient Greek philosopher Aristotle discusses and refutes several popular theories about the oceans, such as the belief that someday the oceans would all dry up, or that the seas were a sort of natural sweat produced by the Earth when heated by the sun.`,
+
+`The Greek God of the sea was Poseidon, who became Neptune in the Roman tradition. This aquatic overlord wielded a trident and summoned storms to punish or warn human beings.`,
+
+`Legal tradition holds that the oceans are not subject to private or national dominion or ownership. The Roman doctrine of Mare Liberum (freedom of the seas) holds that the seas belong to all, and this has given rise to the "four freedoms" of the sea: freedom of navigation, fishing, laying of cables and pipelines, and overflight.`,
+
+`Oceanography is the study of the world's oceans, and it includes aspects of geography, physics, chemistry, biology, geology, and meteorology. By contrast, hydrography is the study of navigable waters and their physical characteristics.`,
+
+`Oceanographers generally divide the world ocean into three major oceans:  Atlantic, Pacific, and Indian. Some add a fourth—the Arctic.`,
+
+`In 1998, Benoît Lecomte became the first man to swim across the Atlantic Ocean without a kickboard. He swam an average of eight hours a day for 73 days, resting and eating on a boat in between swims. In 2016, he announced plans to swim the Pacific.`,
+
+`The ocean is the main driver of the planet's weather cycles, acting alternately like a heater and air conditioner.`,
+
+`Many sea plants and animals are bioluminescent, which means they produce light chemically within their bodies.`,
+
+`An average liter of seawater contains 38,000 different species of microbial life.`,
+
+`The high seas have seen their fair share of crime and violence. Piracy goes back to the ancient world, but the Caribbean pirates of the 16th and 17th centuries are perhaps the most iconic.`,
+
+`Modern oceanography is considered to have begun in 1872, when HMS Challenger made a grand tour of the world's oceans on a three-and-a-half-year voyage.`,
+
+`Distances on the ocean are measured in nautical miles, which are about 1.15 times the length of a regular mile.`,
+
+`On average, the water in the ocean is 3.5% salt.`,
+
+`There's a lot more than just water in the ocean. Each cubic mile of seawater contains approximately 165 million tons of dissolved solid material, from plant and animal life to geologic material.`,
+
+`Marine biologists have so far discovered 17,000 species thriving in the ocean depths, living entirely without sunlight, receiving energy from underwater thermal vents.`,
+
+`Despite the incredible amount of research done on the coastal seas, it is still unknown how many species live in these shallow waters. Estimates vary from 178,000 to 10 million.`,
+
+`Approximately 15% of the high-quality protein humans eat comes from the ocean. This number is far higher in many parts of Asia, where fish and other seafood make up a larger part of people's diet.`,
+
+`Historically, ocean depths were measured by lowering ropes with weights attached. Echo-sounding technology was first used in 1927.`,
+
+`The Pacific Ocean is the world's deepest, and the Arctic is the shallowest.`,
+
+`The Sargasso Sea is a unique area in the world ocean. This area of the North Atlantic has very clear, warm, deep blue water. It supports very little life except for Sargassum, or gulfweed, from which it gets its name.`,
+
+`The oceans tides, or the cycle of rising and falling sea levels along coasts, generally follow an interval of 12 hours 25 minutes.`,
+
+`Ocean tides are caused by gravitational interaction between the sun, moon, and earth.`,
+
+`People once believed that the sun was the source of all life on Earth. The discovery of living creatures in the extreme depths of the ocean, where no sunlight penetrates, disproved this hypothesis.`,
+
+`The chemical ratio of salt to water in the oceans is fairly consistent across the world, a phenomenon known as "Forchhammer's Principle," after the Danish scientist who discovered it.`,
+
+`In 1519, Spanish explorer Ferdinand Magellan led an expedition of five ships in an attempt to discover a western sea route to the Spice Islands. Four of the five ships were lost, and Magellan himself was killed; but one ship returned home after three years, making it the first vessel to sail all the way around the world.`,
+
+`The giant squid is so elusive that we know very little about it.`,
+
+`The giant squid was long believed to be a creature of myth. However, Japanese photographers captured a live giant squid on film for the first time in 2004.`,
+
+`The Persian Emperor Xerxes, who reigned from 486–465 BC, reportedly had his soldiers whip the sea while insulting it after a wave destroyed a bridge his army was constructing.`,
+
+`The ocean is big business. A mere 14% U.S. counties (all of which border the ocean), make up 45% of the U.S. gross domestic product (GDP).`,
+
+`Three million U.S. jobs, or about 1 in 45, are dependent in some way upon the ocean.`,
+
+`The sea is home to a variety of mythological creatures. The most famous are mermaids, which appear in a number of European myths. While occasionally friendly, these creatures are often dangerous seductresses who lure sailors to their death.`,
+
+`In 1927, aviator Charles Lindbergh made the first successful solo, non-stop flight over the Atlantic Ocean.`,
+
+`The strong and dangerous currents that sometimes pull people out to sea are called "rip currents." The best way to escape the grip of a rip current is to swim parallel to the shore or diagonally away from the current, not directly against it.`,
+
+`A common feature of many mythological traditions is a massive flood held to have happened in the early days of the world. Such floods appear in Sumerian, Jewish, Egyptian, Norse, Babylonian, and Hindu mythology.`,
+
+`The ocean has a romantic quality that humans are uniquely drawn to. Many people throughout history have felt called to a life at sea, and philosophers, poets, and artists have often turned to it as a subject for aesthetic contemplation.`,
+
+`The song of the sea awakens in many people a longing for something nameless and indefinable. (John William Waterhouse)`,
+
+`Jacques-Yves Cousteau and Émile Gagnan invented the scuba regulator in 1942, which allowed for breathing underwater and opened up new vistas for ocean exploration.`,
+
+`In the popular children's book The Five Chinese Brothers, one of the brothers has the amazing ability to hold the entire ocean in his mouth.`,
+
+`The ocean has often served as a sort of communal garbage can for human beings. One recent example of this is the dumping of 777,000 tons of water contaminated by radioactive tritium by the Fukushima power plant in Japan in 2017.`,
+
+`The blue whale is the largest animal that exists, or has ever existed, on Earth. This majestic mammal travels all over the world's oceans.`,
+
+`Jacques-Yves Cousteau built and inhabited the first undersea habitat. He and another oceanaut lived in the underwater structure, named Conshelf One, for a week in 1962.`,
+
+`Sea Red Tide,
+
+Algal blooms can be as devastating to aquatic populations as oil spills.,
+
+Sometimes coastal waters will turn red because they are suddenly overrun by algal growth. These "red tides" can overwhelm the water, killing millions of fish and other marine life in the process.`,
+
+`Hurricanes are born through the interaction of Earth's winds and her oceans. When winds happen upon a pocket of warm sea water, the result is often a massive storm that can sweep inland and devastate coastal areas.`,
+
+`Carrageenan, a seaweed extract, is used as an emulsifier in a variety of common products like peanut butter, toothpaste, and cosmetics.`,
+
+`In Genesis, the first animals God creates are aquatic, which accords with the contemporary scientific view that all life originated in the primordial oceans.`,
+
+`For a long time it was believed that Venus was oceanic like Earth, partly because of the swirling clouds that cover the planet.,
+
+Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul...then I account it high time to get to sea as soon as I can.- Herman Melville,
+
+In his dialogue Timaeus, Plato tells the story of a fantastically wealthy city called Atlantis, which sank into the sea. The legend has inspired treasure and adventure seekers for the past two thousand years.`,
+
+`Norse mythology held that the end of the world would come about through a massive flood caused by Jörmungandr, the great serpent who circles the world.`,
+
+`Tsunamis, or seismic sea waves, are huge, devastating waves caused by earthquakes, underwater landslides, or volcanic eruptions.`,
+
+`Tsunami is a Japanese word derived from the term "great harbor wave."`,
+
+`Sea Tsunami,
+
+Tsunamis have been known to kill upwards of a quarter of a million people and flatten entire coastal cities.,
+
+One of the largest tsunamis in history occurred in 1883, when the Indonesian volcano Krakatoa erupted. The resulting waves, one of which reached 133 feet high, devastated the coasts of Java and Sumatra, killing 36,000 people. One of the waves reportedly carried a boat two miles inland.`,
+
+`One of the most serious dangers associated with deep sea diving is decompression sickness, commonly known as "the bends." If a diver returns too quickly to the surface, bubbles can form in the blood or body tissues, blocking off circulation and leading to pain, paralysis, or even death.`,
+
+`Sir Francis Drake, the infamous sea-dog and beloved personal pirate of Queen Elizabeth I, was the first Englishman to circumnavigate the globe, and he was licensed by the British crown to harass Spanish shipping vessels.`,
+
+`Scottish myths speak of Selkies, magical sea lions who could take human form.`,
+
+`The origins of the modern submarine date all the way back to Ancient Greece. Legend holds that Alexander the Great had his men lower him into the sea in a glass barrel so that he could study fish.`,
+
+`Sunken Ship,
+
+Who knows what lost treasure sits on the sea floor?,
+
+It is estimated that between one million and three million ships have sunk to the bottom of the ocean over the course of human history, less than 1% of which have been salvaged.`,
+
+`The famous French pirate L'Olonnais terrorized the Caribbean seas in the late 1600s. Known for his cruelty, legend holds that he once cut a suspected traitor's heart out and ate it.`,
+
+`Modern submarines first appeared in the early 1600s. William Bourne, a British innkeeper and mathematician proposed the idea, and Dutch inventor Cornelius von Drebble built the first workable submarine.`,
+
+`Submarines have been used for military purposes, to varying degrees of success, since 1776, when a one-man American craft called the "Turtle" unsuccessfully attacked a British warship. Since then, submarines have revolutionized naval battle.`,
+
+`Recent geological evidence suggests that there is a hidden ocean buried in Earth's mantle. The water is trapped inside a substance called ringwoodite, and some believe that there is as much water below the surface as there is in the oceans.`,
+
+`Drinking sea water in large amounts can be deadly for human beings, as the kidneys cannot handle such high levels of sodium.`,
+
+`Ocean Scuba Facts,
+
+Scuba diving has become both a useful oceanographic tool and a popular hobby.,
+
+The scuba system, which has allowed human beings to intimately explore the underwater world, is actually an acronym: S.C.U.B.A. stands for Self-Contained Underwater Breathing Apparatus.`,
+
+`Whaling, or the hunting of the ocean's whales, has a long history. Norwegians hunted whales as early as 2,000 BC, and the practice has deep historical roots among the Japanese and Inuit peoples as well.`,
+
+`Uncegila was the name of a gigantic water snake in the folklore of the Native American Lakota tribe. This monster flooded the land with salt water so that nothing could grow, before being killed by a pair of brave twins.`,
+
+`Whaling reached its peak in the 1800s, when whale oil was heavily used as a power source. The industry declined with the rise of fossil fuels. Concern over diminishing whale populations led many countries to heavily regulate or outlaw the practice in the mid-20th century.`,
+
+`Lightning hits dry land more often than the ocean, but when it does hit the sea, the water can act as a conductor, electrocuting marine life near the surface.`,
+
+`Many science fiction writers wrote stories about human settlement of Venus, believing it to be covered with oceans and capable of sustaining life. When it was discovered in 1968 that there were no oceans on our sister planet, sci-fi author Brian Aldiss released a compilation of stories entitled Farewell, Fantastic Venus!`,
+
+`The tuna-fishing industry harms many creatures besides the fish. It is estimated that for every 1000 tuna nets used, approximately 654 billfish, 102 sea turtles, 13,958 sharks, and 2 dolphins are killed.`,
+
+`Shrimp fishing boats, or trawlers, kill and dump two to four times more shrimp than they catch and keep.`,
+
+`Sea Nautical Chart,
+
+It takes special training to read a map like this.,
+
+Sailors use unique maps of the ocean called "nautical charts." These charts provide specific information about ocean depth, anchorages, and coastline shape and configuration.`,
+
+`Perhaps the most famous European sea monster, the kraken, is a favorite pirate myth. The giant squid-like creature was said to live off of the Norwegian coast.`,
+
+`Unusually high tides are known as "king tides."`,
+
+`The deepest part of the ocean is the Marianas Trench, which is over 36,000 feet deep. If Mount Everest were dropped into the trench, its peak would still be over a mile underwater.`,
+
+`The atmospheric pressure at the bottom of the Marianas Trench is roughly 1,000 times what it is at the surface.`,
+
+`Human beings have been to the bottom of the Marianas Trench—the deepest point in the world's oceans—only twice. In 1960, Jacques Piccard and Don Walsh descended in the naval bathyscaphe Trieste. In 2012, James Cameron made the dive solo in his vertical torpedo sub.`,
+
+`To date, human beings have only explored 5% of Earth's oceans.`,
+
+`The Bermuda Triangle is an infamous area of the Atlantic, between Miami, Bermuda, and Puerto Rico. Though many ships and planes have been lost in this particular section of the sea, there is really no evidence that accidents are more frequent here than in other heavily trafficked areas of the sea.`,
+
+`Whale ocean fact,
+
+Given how little we know about the ocean, we may see some monsters yet.,
+
+Every now and then people discover giant, unknown corpses on shore. While these are sometimes fearfully called sea monsters, they are often dead Basking Sharks, which decay quickly, leaving an eerie skeleton.`,
+
+`Legend holds that deep-sea fish will explode when brought to the surface, but this is false. Most deep-sea fish have no swim bladders, so they are unaffected by such changes in pressure.`,
+
+`Moby Dick, the famous white whale from Herman Melville's classic novel, isn't so far-fetched. Albino killer whales have been sighted, and it's possible that the sea might hold a giant white sperm whale just like Ahab's nemesis.`,
+
+`Edward Teach was an English pirate better known as Blackbeard. This fearsome dog of the deep terrified his foes by lacing smoking fuses into his beard and wearing multiple daggers and pistols on his chest.`,
+
+`Human interaction with the ocean often upsets natural ecosystems. For instance, foreign aquatic plants or animals have been introduced into waters that cannot sustain them or that suffer from their presence.`,
+
+`On his voyage to the New World, Christopher Columbus reported seeing a great ball of fire sinking into the area now known as the Bermuda Triangle. Many speculate that what he saw was a meteor.`,
+
+`People love the beach. In California alone, ocean-seeking tourists spend 75 billion dollars annually.`,
+
+`Beach Ocean Sunbathing,
+
+People in some parts of the world survive almost entirely on beach tourism revenue.,
+
+The Gulf Stream is a series of currents converging in the sea, which can propel animals and boats along at a speed more rapid than that of the Mississippi or Amazon Rivers.`,
+
+`There's a lot of gold in the ocean, both dissolved in the water and on the sea floor. Unfortunately, there's no economically feasible way of mining it. If we could, there'd be enough for each person on earth to have nine pounds of pure gold.`,
+
+]
+
+
+let randomFactIndex = Math.floor(Math.random(0,100)*100);
+console.log(facts[randomFactIndex]);
+
+let factsArea = document.getElementsByClassName("facts")[0];
+factsArea.innerHTML = facts[randomFactIndex];
+factsArea.style.color = "blue"
+factsArea.style.background = "yellow"
+
+
